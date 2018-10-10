d70f998c769e9c9a2774af05c353d6647f73b77b
= <=>
2018-10-10 09:13:23 +0700

    update bài tập 5 done

---
+++ src/exercise5_Triangle/ShapeTest.java
@@ -0,0 +1,8 @@
+package exercise5_Triangle;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(10, 12, 7.5);
+        System.out.println(triangle);
+    }
+}
